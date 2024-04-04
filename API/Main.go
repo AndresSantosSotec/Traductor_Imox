b8e90f6af2137991130d384d5ce635e5ac2836a8
AndresDev <mlpdbz300@gmail.com>
2024-04-03 22:53:37 -0600

    Cambios de Isaac aplicados

--- API/Main.go
+++ API/Main.go
@@ -16,6 +16,7 @@ type Traduccion struct {
     ID           int    `json:"id"`
     PalabraMaya  string `json:"palabra_maya"`
     PalabraEsp   string `json:"palabra_esp"`
+    PalabraQeqchi string `json:"palabra_qeqchi"`
 }
 
 func main() {
@@ -47,8 +48,21 @@ func main() {
 }
 
 func getTraducciones(w http.ResponseWriter, r *http.Request) {
-    // Consulta las traducciones en la base de datos
-    rows, err := db.Query("SELECT id, palabra_maya, palabra_esp FROM traducciones")
+    // Obtiene el idioma solicitado de la consulta
+    idioma := r.URL.Query().Get("idioma")
+
+    // Consulta las traducciones en la base de datos según el idioma especificado
+    var rows *sql.Rows
+    var err error
+    switch idioma {
+    case "qeqchi":
+        rows, err = db.Query("SELECT id, palabra_maya, palabra_qeqchi FROM traducciones")
+    case "espanol":
+        rows, err = db.Query("SELECT id, palabra_maya, palabra_esp FROM traducciones")
+    default:
+        http.Error(w, "Idioma no válido", http.StatusBadRequest)
+        return
+    }
     if err != nil {
         log.Fatal(err)
     }
@@ -58,8 +72,15 @@ func getTraducciones(w http.ResponseWriter, r *http.Request) {
     var traducciones []Traduccion
     for rows.Next() {
         var traduccion Traduccion
-        if err := rows.Scan(&traduccion.ID, &traduccion.PalabraMaya, &traduccion.PalabraEsp); err != nil {
-            log.Fatal(err)
+        switch idioma {
+        case "qeqchi":
+            if err := rows.Scan(&traduccion.ID, &traduccion.PalabraMaya, &traduccion.PalabraQeqchi); err != nil {
+                log.Fatal(err)
+            }
+        case "espanol":
+            if err := rows.Scan(&traduccion.ID, &traduccion.PalabraMaya, &traduccion.PalabraEsp); err != nil {
+                log.Fatal(err)
+            }
         }
         traducciones = append(traducciones, traduccion)
     }
